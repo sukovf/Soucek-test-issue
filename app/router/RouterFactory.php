fec1174adf55c977e7bbdfe736c74c9bd686b290
sukovf <sukovf@users.noreply.github.com>
2017-07-17 21:46:50 +0200

    Minor improvement
    Minor improvement

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -18,6 +18,7 @@ class RouterFactory
 	{
 		$router = new RouteList;
     $router[] = new Route("test-issue", "Homepage:default");
+    $router[] = new Route("<presenter>/<action>", "Homepage:default");
 		return $router;
 	}
 
