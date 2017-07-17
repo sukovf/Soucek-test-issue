1a28526136d8b3e67c929ad8fb4b1709cfbcb98e
sukovf <sukovf@users.noreply.github.com>
2017-07-17 21:48:02 +0200

    Update RouterFactory.php

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -17,8 +17,8 @@ class RouterFactory
 	public static function createRouter()
 	{
 		$router = new RouteList;
-    $router[] = new Route("test-issue", "Homepage:default");
-    $router[] = new Route("<presenter>/<action>", "Homepage:default");
+    		$router[] = new Route("test-issue", "Homepage:default");
+    		$router[] = new Route("<presenter>/<action>", "Homepage:default");
 		return $router;
 	}
 
